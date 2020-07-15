619ab45224e7dab78778d87c1b7360599a91f6b4
sewoop <raku@hotmail.co.kr>
2020-07-15 16:54:30 +0900

    ALGO 200715

--- Baekjoon/Step 01_12/10872.py
+++ Baekjoon/Step 01_12/10872.py
@@ -1,1 +1,7 @@
 # 10872.py
+
+n, mul = int(input()), 1
+for i in range(1, n+1):
+    mul *= i
+
+print(mul)
---
+++ Baekjoon/Step 01_12/2530.py
@@ -0,0 +1,13 @@
+# 2530.py
+
+H,M,S = map(int, input().split())
+D = int(input())
+
+S+=D
+M+=S//60
+H+=M//60
+S=S%60
+M=M%60
+H=H%24
+
+print(H,M,S)
