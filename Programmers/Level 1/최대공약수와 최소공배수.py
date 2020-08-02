987328541070c7534e3cdedae7c1d91472835fa2
sewoop <raku@hotmail.co.kr>
2020-08-02 22:19:11 +0900

    Add (#) - 프로그래머스 최대공약수와 최소공배수 문제

--- Programmers/Level 1/최대공약수와 최소공배수.py
+++ Programmers/Level 1/최대공약수와 최소공배수.py
@@ -1,3 +1,18 @@
+# math module를 사용
+import math
 def solution(n, m):
-    answer = []
-    return answer
+    return [math.gcd(n,m), n*m//math.gcd(n,m)]
+
+# math 없이
+def solution(n, m):
+    gcd = lambda a,b: b if not a%b else gcd(b, a%b)
+    lcm = lambda a,b: a*b//gcd(a,b)
+
+    return [gcd(n,m), lcm(n,m)]
+
+if __name__ == "__main__":
+    n = 12
+    m = 18
+    # print(solution(n,m))
+
+    print(18%12)
