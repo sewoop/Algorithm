73d31a18bd1c8a28ffdeced4b9ab3d4907858440
sewoop <raku@hotmail.co.kr>
2020-08-02 09:04:17 +0900

    Add (#) - 프로그래머스 완주하지 못한 선수 문제

---
+++ Programmers/Level 1/완주하지 못한 선수.py
@@ -0,0 +1,25 @@
+# 효율성 문제 발생
+def solution(participant, completion):
+    for comp in completion:
+        index = participant.index(comp)
+        participant.pop(index)
+
+    return ''.join(participant)
+
+# Collections 사용
+import collections
+
+def solution(participant, completion):
+    answer = collections.Counter(participant) - collections.Counter(completion)
+    return list(answer.keys())[0]
+
+if __name__ == "__main__":
+    # participant = ['leo', 'kiki', 'eden']
+    # completion = ['eden', 'kiki']
+    
+    # participant = ['marina', 'josipa', 'nikola', 'vinko', 'filipa']
+    # completion = ['josipa', 'filipa', 'marina', 'nikola']
+
+    participant = ['mislav', 'stanko', 'mislav', 'ana']
+    completion = ['stanko', 'ana', 'mislav']
+    print(solution(participant, completion))
