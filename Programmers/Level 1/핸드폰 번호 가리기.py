7564ab7b7386925e296857164a335e6ec7d46b2b
sewoop <raku@hotmail.co.kr>
2020-08-03 01:33:12 +0900

    Add (#) - 프로그래머스 핸드폰 번호 가리기 문제

---
+++ Programmers/Level 1/핸드폰 번호 가리기.py
@@ -0,0 +1,14 @@
+def solution(phone_number):    
+    phone = [i for i in phone_number]
+    
+    for i in range(len(phone)-1,-1,-1):
+        if i < len(phone)-4:
+            phone[i]='*'
+
+    return ''.join(phone)
+
+if __name__ == "__main__":
+    # phone_number = "01033334444"
+    phone_number = "027778888"
+
+    print(solution(phone_number))
