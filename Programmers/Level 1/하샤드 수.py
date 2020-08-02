ca6f9be1b0fd41b41dc9b54a30c9d83c80972b52
sewoop <raku@hotmail.co.kr>
2020-08-03 02:51:32 +0900

    Modify (#) - 하샤드 수 문제

--- Programmers/Level 1/하샤드 수.py
+++ Programmers/Level 1/하샤드 수.py
@@ -2,6 +2,9 @@ def solution(x):
     lists = [i for i in str(x)]
     return True if int(''.join(lists))%sum(map(int, lists))==0 else False
 
+def solution(x):
+    return x%sum([int(c) for c in str(x)])==0
+
 if __name__ == "__main__":
     arr = 18
     print(solution(arr))
