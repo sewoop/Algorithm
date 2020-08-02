65f6c2c75e074b093869595c7a63aa68143a05ca
sewoop <raku@hotmail.co.kr>
2020-08-02 13:23:17 +0900

    Add (#) - 프로그래머스 소수 찾기 문제

---
+++ Programmers/Level 1/소수 찾기.py
@@ -0,0 +1,28 @@
+# 에라토스테네스의 체를 사용하여 구하기
+def solution(n):
+    num = set(range(2,n+1))
+    # print(num)
+    for i in range(2,n+1):
+        if i in num:
+            num-=set(range(2*i,n+1,i))
+            # print(f'{num} {set(range(2*i,n+1,i))}')
+    return len(num)
+
+
+if __name__ == "__main__":
+    n = 10
+    print(solution(n))
+
+
+'''
+에라토스테네스의 체를 가장 잘 나타낸 코드
+
+# 2 ~ n 
+num = set(range(2,n+1))
+
+for i in range(2,n+1):
+    if i in num:
+        num-=set(range(2*i, n+1, i))
+
+return len(num)
+'''
