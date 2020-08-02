67fa72e25588d82395ab25b0bd3cbd249a8b103a
sewoop <raku@hotmail.co.kr>
2020-08-02 11:35:00 +0900

    Modify (#) - 프로그래머스 체육복 문제 수정

--- Programmers/Level 1/체육복.py
+++ Programmers/Level 1/체육복.py
@@ -1,25 +1,46 @@
 def solution(n, lost, reserve):
-    reserve = set(reserve)-set(lost)
-    lost = set(lost)-set(reserve)
+    reserve_list = set(reserve)-set(lost)
+    lost_list = set(lost)-set(reserve)
 
-    for i in reserve:
-        if i-1 in lost:
-            lost.remove(i-1)
-        elif i+1 in lost:
-            lost.remove(i+1)
+    # print(reserve_list)
+    # print(lost_list)
 
-    return n-len(lost)
+    for i in reserve_list:
+        if i-1 in lost_list:
+            lost_list.remove(i-1)
+        elif i+1 in lost_list:
+            lost_list.remove(i+1)
 
-# n=5
-# lost=[2, 4]
-# reserve=[1, 3, 5]
+    return n-len(lost_list)
 
-# n=5
-# lost=[2, 4]
-# reserve=[3]
 
-n=3
-lost=[3]
-reserve=[1]
+if __name__ == "__main__":
+    # n=5
+    # lost=[2, 4]
+    # reserve=[1, 3, 5]
 
-print(solution(n,lost,reserve))
+    # n=5
+    # lost=[2, 4]
+    # reserve=[3]
+
+    # n=3
+    # lost=[3]
+    # reserve=[1]
+
+    # n = 5
+    # lost = [1, 3]
+    # reserve = [2, 4]
+
+    # n = 5
+    # lost = [3, 5]
+    # reserve = [2, 4]
+
+    # n = 8
+    # lost = [2,3,4]
+    # reserve = [1]
+
+    n = 8
+    lost = [5,6]
+    reserve = [4,5]
+
+    print(solution(n,lost,reserve))
