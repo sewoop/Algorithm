a709e3f7b80dc6ea75e99f0e8683ffef33a45251
sewoop <raku@hotmail.co.kr>
2020-08-02 21:00:36 +0900

    Add (#) - 프로그래머스 약수의 합 문제

---
+++ Programmers/Level 1/약수의 합.py
@@ -0,0 +1,14 @@
+def solution(n):
+    answer = 0
+
+    num = set(range(1,n+1))
+
+    for i in range(1, n+1):
+        if i in num and n%i==0:
+            answer+=i
+
+    return answer
+
+if __name__ == "__main__":
+    n = 12
+    print(solution(n))
