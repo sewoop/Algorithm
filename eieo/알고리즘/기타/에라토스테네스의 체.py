f32d1fa8edf79a17dbac62b0a3ba7c711ce36289
sewoop <raku@hotmail.co.kr>
2020-12-21 16:25:06 +0900

    algo: 20201221

--- eieo/알고리즘/기타/에라토스테네스의 체.py
+++ eieo/알고리즘/기타/에라토스테네스의 체.py
@@ -1,16 +1,17 @@
 import math
+from typing import List
 
 # n: max 값
-def is_prime(n: int) -> list:
+def isPrime(maxValue: int) -> List:
     # n = 1000 # 2부터 1,000까지의 모든 수에 대하여 소수 판별
-    prime_list = [True for _ in range(n + 1)] # 모든 수를 True로 초기화
+    prime_list = [True for _ in range(maxValue + 1)] # 모든 수를 True로 초기화
 
     # 에라토스테네스의 체 알고리즘
-    for i in range(2, int(math.sqrt(n)) + 1): # 2부터 n의 제곱근까지의 모든 수를 확인
-        if prime_list[i] == True # i가 소수인 경우
+    for i in range(2, int(math.sqrt(maxValue)) + 1): # 2부터 n의 제곱근까지의 모든 수를 확인
+        if prime_list[i] == True: # i가 소수인 경우
             # i를 제외한 i의 모든 배수를 지우기
             j = 2
-            while i * j <= n:
+            while i * j <= maxValue:
                 prime_list[i * j] = False
                 j += 1
 
