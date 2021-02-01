7618fcb3c63220ed5034579e3bc43b61e7bfc4d8
sewoop <raku@hotmail.co.kr>
2021-02-01 19:35:24 +0900

    algo: 백준 10773 제로

---
+++ Baekjoon/유형별 분류/스택/10773 제로.py
@@ -0,0 +1,14 @@
+import sys
+
+k = int(sys.stdin.readline())
+
+stack = []
+
+for _ in range(k):
+    temp = int(sys.stdin.readline())
+    if temp:
+        stack.append(temp)
+    else:
+        stack.pop()
+
+print(sum(stack))
