c21387265c26372843c88f32b25bd1b8c16a9f97
sewoop <raku@hotmail.co.kr>
2021-02-05 00:38:26 +0900

    algo: 백준 15651 백트래킹 복습

--- Baekjoon/유형별 분류/백트래킹/15651 N과 M (3).py
+++ Baekjoon/유형별 분류/백트래킹/15651 N과 M (3).py
@@ -1,3 +1,28 @@
+n, m = map(int, input().split())
+
+data = [0] * m
+used = [0] * (n + 1)
+
+def backtrack(index):
+    if index == m:
+        for i in range(m):
+            print(data[i], end=' ')
+        print()
+        return
+    
+    for i in range(1, n + 1):
+        if not used[i]:
+            data[index] = i
+            for j in range(i):
+                used[j] = 1
+            backtrack(index + 1)
+            for j in range(i):
+                used[j] = 0
+
+backtrack(0)
+
+
+'''
 N, M = map(int, input().split())
 
 data = [0] * (N + 1)
@@ -18,3 +43,4 @@ def backTracking(index):
         isUsed[i] = 0
 
 backTracking(0)
+'''
