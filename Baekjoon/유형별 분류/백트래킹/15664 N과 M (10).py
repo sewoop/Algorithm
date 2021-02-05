5c1771d9cd918295a47705c8ac1eacdb5e4b4154
sewoop <raku@hotmail.co.kr>
2021-02-05 13:24:22 +0900

    algo: 백준 15664 N과 M (10)

---
+++ Baekjoon/유형별 분류/백트래킹/15664 N과 M (10).py
@@ -0,0 +1,31 @@
+import sys
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+numbers = sorted(map(int, input().split()))
+
+data = [0] * m
+used = [0] * (n + 1)
+
+result = []
+
+def backtrack(index):
+    if index == m:    
+        result.append(tuple(data[:m]))
+        return
+    
+    for i in range(1, n + 1):
+        if not used[i]:
+            data[index] = numbers[i - 1]
+            for j in range(i + 1):
+                used[j] = 1
+            backtrack(index + 1)
+            for j in range(i + 1):
+                used[j] = 0
+
+backtrack(0)
+
+for number in sorted(set(result)):
+    for num in number:
+        print(num, end=' ')
+    print()
