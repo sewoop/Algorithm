d4efc30eb34133ab1f1258a922adca8d333540d9
sewoop <raku@hotmail.co.kr>
2021-02-05 13:24:12 +0900

    algo: 백준 15663 N과 M (9)

--- Baekjoon/유형별 분류/백트래킹/15663 N과 M (9).py
+++ Baekjoon/유형별 분류/백트래킹/15663 N과 M (9).py
@@ -1,3 +1,24 @@
+## itertools
+# import sys
+# from itertools import permutations
+# input = sys.stdin.readline
+
+# n, m = map(int, input().split())
+# numbers = sorted(map(int, input().split()))
+
+# result = []
+
+# for number in list(permutations(numbers, m)):
+#     result.append(number)
+
+# result = sorted(list(set(result)))
+
+# for number in result:
+#     for num in number:
+#         print(num, end=' ')
+#     print()
+
+## direct
 import sys
 input = sys.stdin.readline
 
@@ -7,11 +28,11 @@ numbers = sorted(map(int, input().split()))
 data = [0] * m
 used = [0] * (n + 1)
 
+result = []
+
 def backtrack(index):
-    if index == m:
-        for i in range(m):
-            print(data[i], end=' ')
-        print()
+    if index == m:    
+        result.append(tuple(data[:m]))
         return
     
     for i in range(1, n + 1):
@@ -22,3 +43,8 @@ def backtrack(index):
             used[i] = 0
 
 backtrack(0)
+
+for number in sorted(set(result)):
+    for num in number:
+        print(num, end=' ')
+    print()
