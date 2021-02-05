3b6d3e99b4ffb045d1a7b0e76e62b23fb9d202b5
sewoop <raku@hotmail.co.kr>
2021-02-05 15:39:59 +0900

    algo: 백준 14888 연산자 끼워넣기

---
+++ Baekjoon/유형별 분류/백트래킹/14888 연산자 끼워넣기.py
@@ -0,0 +1,46 @@
+import sys
+input = sys.stdin.readline
+import math
+
+n = int(input())
+numbers = list(map(int, input().split()))
+operators = list(map(int, input().split()))
+
+result = []
+total = 0
+
+def backtrack(index):
+    global total, result
+
+    if index == n:
+        result.append(total)
+        return
+
+    if index == 0:
+        total += numbers[index]
+        backtrack(index + 1)
+        return
+
+    for i in range(4): # 0, 1, 2, 3
+        if operators[i] != 0:
+            operators[i] -= 1
+            temp = total
+            if i == 0:
+                total = temp + numbers[index]
+                backtrack(index + 1)
+            elif i == 1:
+                total = temp - numbers[index]
+                backtrack(index + 1)
+            elif i == 2:
+                total = temp * numbers[index]
+                backtrack(index + 1)
+            else:
+                total = math.trunc(temp / numbers[index])
+                backtrack(index + 1)
+            total = temp
+            operators[i] += 1
+
+backtrack(0)
+
+print(max(result))
+print(min(result))
