f331244cc1afe041c9d522368181f69c198faafd
sewoop <raku@hotmail.co.kr>
2021-02-23 02:25:25 +0900

    algo: [1주차] 프로그래머스 - 프린터

---
+++ Programmers/Level 2/[1주차] 프린터.py
@@ -0,0 +1,27 @@
+def solution(priorities, location):
+    answer = 0
+
+    prior = sorted(priorities)
+    documents = [(i, v) for i, v in enumerate(priorities)]
+
+    lst = []
+
+    while prior:
+        maximum = max(prior)
+        for index, document in enumerate(documents):
+            if document[1] == maximum:
+                documents = documents[index:] + documents[:index]
+                break
+
+        lst.append(documents.pop(0))
+        prior.pop()
+
+    for i, v in enumerate(lst):
+        if v[0] == location:
+            answer = i + 1
+            break      
+
+    return answer
+
+print(solution([2, 1, 3, 2], 2))
+print(solution([1, 1, 9, 1, 1, 1], 0))
--- README.md
+++ README.md
@@ -107,7 +107,7 @@
 * [x] [이론 학습 - Queue, Deque](https://gmlwjd9405.github.io/2018/08/02/data-structure-queue.html)
 * [x] [프로그래머스 - 기능개발](https://programmers.co.kr/learn/courses/30/lessons/42586)
 * [x] [프로그래머스 - 다리를 지나는 트럭](https://programmers.co.kr/learn/courses/30/lessons/42583)
-* [프로그래머스 - 프린터](https://programmers.co.kr/learn/courses/30/lessons/42587)
+* [x] [프로그래머스 - 프린터](https://programmers.co.kr/learn/courses/30/lessons/42587)
 
 ### 2주차
 * [이론 학습 - DFS](https://gmlwjd9405.github.io/2018/08/14/algorithm-dfs.html)
