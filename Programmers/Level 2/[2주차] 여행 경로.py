226f919ede4d55bb4d6e80694411566f7925490e
sewoop <raku@hotmail.co.kr>
2021-03-06 19:53:36 +0900

    algo: [2주차] 프로그래머스 - 여행 경로

--- Programmers/Level 2/[2주차] 여행 경로.py
+++ Programmers/Level 2/[2주차] 여행 경로.py
@@ -1,33 +1,42 @@
+from collections import defaultdict
+
 def solution(tickets):
-    answer = []
+    def init_graph():
+        routes = defaultdict(list)
+        for key, value in tickets:
+            routes[key].append(value)
 
-    graph = {}
-    for ticket in tickets:
-        if ticket[0] not in graph.keys():
-            graph[ticket[0]] = [ticket[1]]
-        else:
-            graph[ticket[0]].append(ticket[1])
-            graph[ticket[0]].sort()
+        for r in routes:
+            routes[r].sort()
 
-    print(graph)
+        return routes
 
-    start = "ICN"
-    queue = [start]
+    # DFS
+    def nextRoute():
+        stack = ['ICN']
+        path = [] # 가려고하는 경로를 저장하는 변수
 
-    while queue:
-        node = queue.pop(0)
-        answer.append(node)
+        while stack:
+            top = stack[-1]
 
-        if node in graph.keys() and len(graph[node]) > 0:
-            queue.append(graph[node].pop(0))
-        
-        print(queue)
+            # 특정 공항에서 출발하는 표가 없다면 또는 있지만 티켓을 다 써버린 경우
+            if top not in routes or len(routes[top]) == 0:
+                path.append(stack.pop())
+            else:
+                stack.append(routes[top].pop(0))
+        return path[::-1]
 
+    routes = init_graph()
+    answer = nextRoute()
+    
     return answer
 
 # tickets = [["ICN", "JFK"], ["HND", "IAD"], ["JFK", "HND"]]
 # tickets = [["ICN", "SFO"], ["ICN", "ATL"], ["SFO", "ATL"], ["ATL", "ICN"], ["ATL","SFO"]]
 # tickets = [["ICN", "BOO"], ["ICN", "COO"], ["COO", "DOO"], ["DOO", "COO"], ["BOO", "DOO"],["DOO", "BOO"], ["BOO", "ICN"], ["COO", "BOO"]]
-# tickets = [["ICN", "COO"], ["COO", "ICN"],["COO", "ICN"]]
-tickets =  [['ICN','BOO' ], [ 'ICN', 'COO' ], [ 'COO', 'DOO' ], ['DOO', 'COO'], [ 'BOO', 'DOO'] ,['DOO', 'BOO'], ['BOO', 'ICN' ], ['COO', 'BOO']]
+# tickets =  [['ICN','BOO' ], [ 'ICN', 'COO' ], [ 'COO', 'DOO' ], ['DOO', 'COO'], [ 'BOO', 'DOO'] ,['DOO', 'BOO'], ['BOO', 'ICN' ], ['COO', 'BOO']]
+# tickets = [['ICN', 'A'], ['A', 'C'], ['A', 'D'], ['D', 'K']]
+tickets = [['ICN', 'A'], ['ICN', 'B'], ['B', 'ICN']]
+# tickets = [['ICN', 'A'], ['ICN', 'A'], ['A', 'ICN'], ['A' , 'C']]
+# tickets = [['ICN','B'],['B','ICN'],['ICN','A'],['A','D'],['D','A']]
 print(solution(tickets))
