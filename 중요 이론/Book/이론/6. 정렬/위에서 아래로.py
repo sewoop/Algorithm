dd0d766e7cd00d77ff4c0b9b1859dc0ee6d395ba
sewoop <raku@hotmail.co.kr>
2021-03-28 18:24:03 +0900

    algo: 이취코 - 6. 정렬: 위에서 아래로

---
+++ 중요 이론/Book/이론/6. 정렬/위에서 아래로.py
@@ -0,0 +1,19 @@
+def solution():
+    n = int(input())
+
+    data = []
+    for _ in range(n):
+        data.append(int(input()))
+    
+    data.sort(reverse=True)
+
+    return data
+
+print(*solution())
+
+'''
+3
+15 
+27
+12
+'''
