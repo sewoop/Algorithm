6ec3cccb1296454b7da5805a08dd549ec02b9b59
sewoop <raku@hotmail.co.kr>
2021-03-28 18:31:20 +0900

    algo: 이취코 - 6. 정렬: 성적이 낮은 순서로 학생 출력하기

---
+++ 중요 이론/Book/이론/6. 정렬/성적이 낮은 순서로 학생 출력하기.py
@@ -0,0 +1,23 @@
+def solution():
+    n = int(input())
+
+    data = []
+    for _ in range(n):
+        name, score = input().split()
+        data.append((name, int(score)))
+
+    data.sort(key=lambda x: x[1])
+
+    result = []
+    for i in data:
+        result.append(i[0])
+
+    return result
+
+print(*solution())
+
+'''
+2
+홍길동 95
+이순신 77
+'''
