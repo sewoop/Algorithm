9106140271693fa54dbb79f82863b39be730324b
seu0313 <raku@hotmail.co.kr>
2021-04-22 17:16:58 +0900

    algo: 프로그래머스 - 베스트 앨범

---
+++ Programmers/Level 3/[9주차] 베스트앨범.py
@@ -0,0 +1,56 @@
+from collections import defaultdict
+from functools import reduce
+
+def solution_0(genres, plays):
+    answer = []
+    album = defaultdict(list)
+
+    for i, v in enumerate(zip(genres, plays)):
+        album[v[0]].append((v[1], i))
+
+    album_list = []
+    for genre in set(genres):
+        album[genre].sort(key=lambda x: (-x[0], x[1]))
+        temp = reduce(lambda x, y: x + y[0], album[genre], 0)
+        album_list.append((temp, album[genre]))
+
+    album_list.sort(reverse=True)
+
+    for i in album_list:
+        for idx, j in enumerate(i[1]):
+            if idx < 2:
+                answer.append(j[1])
+
+    return answer
+
+def solution(genres, plays):
+    answer = []
+
+    album = {}
+    for i, v in enumerate(zip(genres, plays)):
+        if v[0] not in album:
+            album[v[0]] = [v[1], [(v[1], i)]]
+        else:
+            album[v[0]][0] += v[1]
+            album[v[0]][1].append((v[1], i))
+
+    for genre in set(genres):
+        album[genre][1].sort(key=lambda x: (-x[0], x[1]))
+
+    album_list = list(album.values())
+    album_list.sort(reverse=True)
+
+    for i in album_list:
+        for j in i[1][:2]:
+            answer.append(j[1])
+
+    return answer
+
+
+genres = ["classic", "pop", "classic", "classic", "pop"]
+plays = [500, 600, 150, 800, 2500]
+
+# genres = ["classic", "classic", "classic", "pop"]
+# plays = [500, 200, 200, 2500]
+
+print(solution(genres, plays))
--- README.md
+++ README.md
@@ -200,7 +200,7 @@
 - [x] [이론 학습 - Trie]()
 - [x] [프로그래머스 - 전화번호 목록](https://programmers.co.kr/learn/courses/30/lessons/42577)
 - [x] [프로그래머스 - 위장](https://programmers.co.kr/learn/courses/30/lessons/42578)
-- [ ] [프로그래머스 - 베스트앨범](https://programmers.co.kr/learn/courses/30/lessons/42579)
+- [x] [프로그래머스 - 베스트앨범](https://programmers.co.kr/learn/courses/30/lessons/42579)
 - [ ] [프로그래머스 - 더 맵게](https://programmers.co.kr/learn/courses/30/lessons/42626)
 - [ ] [프로그래머스 - 디스크 컨트롤러](https://programmers.co.kr/learn/courses/30/lessons/42627)
 - [ ] [프로그래머스 - 이중 우선순위 큐](https://programmers.co.kr/learn/courses/30/lessons/42628)
