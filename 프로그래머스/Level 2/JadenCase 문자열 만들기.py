4e8f1066d1f58fe202a10abff27604a6fd10381d
sewoop <raku@hotmail.co.kr>
2021-05-05 02:18:00 +0900

    feat: 프로그래머스 - JadenCase 문자열 만들기

---
+++ 프로그래머스/Level 2/JadenCase 문자열 만들기.py
@@ -0,0 +1,25 @@
+# sol1
+def solution(s):
+    string = ""
+    flag = False
+    for i in s:
+        if i == " ":
+            flag = False
+        else:
+            if not flag:
+                flag = True
+                string += i.upper()
+                continue
+        string += i.lower()
+    return string
+
+# sol2
+def solution(s):
+    return s.title() # 개편되어 사용 불가
+
+# sol3
+def solution(s):
+    return ' '.join([word.capitalize() for word in s.split(" ")])
+
+s = "3people unFollowed me    for the last week"
+print(solution(s))
