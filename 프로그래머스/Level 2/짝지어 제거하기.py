8c836a42e72bf7af01928b6b29c83f3a297723f1
sewoop <raku@hotmail.co.kr>
2021-05-04 11:23:53 +0900

    feat: 프로그래머스 - 짝지어 제거하기

---
+++ 프로그래머스/Level 2/짝지어 제거하기.py
@@ -0,0 +1,15 @@
+def solution(s):
+    stack = []
+    for i in s:
+        if not stack or stack[-1] != i:
+            stack.append(i)
+            continue
+        if stack[-1] == i:
+            stack.pop()
+    return 0 if stack else 1
+
+s = "baabaa"
+s = "cdcd"
+s = "c"
+s = ""
+print(solution(s))
