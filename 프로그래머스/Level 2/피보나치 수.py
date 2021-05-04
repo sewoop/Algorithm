44577d490ee5b479abe95ab9591039ad86fbcda4
sewoop <raku@hotmail.co.kr>
2021-05-04 13:22:11 +0900

    feat: 프로그래머스 - 피보나치 수

---
+++ 프로그래머스/Level 2/피보나치 수.py
@@ -0,0 +1,14 @@
+# def solution(n):
+#     dp = [0] * (n + 1)
+#     dp[1] = dp[2] = 1 
+#     for i in range(3, n + 1):
+#         dp[i] = dp[i - 2] + dp[i - 1]
+#     return dp[n] % 1234567
+
+def solution(n):
+    a, b = 0, 1 
+    for i in range(1, n):
+        a, b = b, a + b
+    return b % 1234567
+
+print(solution(3))
