18e9b012fafe4580aff4921363bc87b9fa6afdee
sewoop <raku@hotmail.co.kr>
2021-05-06 00:19:43 +0900

    feat: 프로그래머스 - N개의 최소공배수

---
+++ 프로그래머스/Level 2/N개의 최소공배수.py
@@ -0,0 +1,14 @@
+def solution(arr):
+	def gcd(a, b):
+		while b > 0:
+			a, b = b, a % b
+		return a
+
+	def lcm(a, b):
+		return (a * b) // gcd(a, b)
+						
+	answer = arr[0]
+	for i in range(1, len(arr)):
+		answer = lcm(answer, arr[i])
+	
+	return answer
