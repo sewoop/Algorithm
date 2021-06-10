3e693afdbd61f11228e038450d3494e9862ba7b3
sewoop <raku@hotmail.co.kr>
2021-06-10 10:37:23 +0900

    feat: 백준 - 1697 숨바꼭질, 7576 토마토, 폴더명 변경

---
+++ 백준/분류별/그래프 탐색/1697 숨바꼭질.py
@@ -0,0 +1,20 @@
+from collections import deque
+
+MAX = int(10 ** 5)
+n, k = map(int, input().split())			
+dist = [0] * (MAX + 1)
+
+def bfs():
+	queue = deque([n])
+	
+	while queue:
+		x = queue.popleft()
+		
+		if x == k: return dist[x]
+		
+		for nx in [x - 1, x + 1, x * 2]:
+			if 0 <= nx <= MAX and not dist[nx]:
+				dist[nx] = dist[x] + 1
+				queue.append(nx)
+				
+print(bfs())
